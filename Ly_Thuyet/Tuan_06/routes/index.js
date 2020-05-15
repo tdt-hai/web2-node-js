0c27abb1bd801d2fcd27309a4c09380fccd067b9
Tran Do Thanh Hai <57478186+tdt-hai@users.noreply.github.com>
2020-05-15 19:39:06 +0700

    add W6

---
+++ Ly_Thuyet/Tuan_06/routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+const user = require('../services/user');
+const todo = require('../services/todo');
+
+const asyncHandler = require('express-async-handler');
+
+/* GET home page. */
+router.get('/', asyncHandler (async function(req, res, next) {
+  const title = "BTCN06-Todo app";
+  var today = new Date();
+  var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+  var id = await user.finduserbyid(req.session.id);
+  if(id)
+  {
+    //Xuất danh sách todo chưa hoàn thành
+    const exportTodo = await todo.findallnotdone(req.session.id);
+    //Xuất danh sách công việc đã hoàn thành
+    const complete = await todo.findalldone(req.session.id);
+    res.render('index', { id,title,date,exportTodo,complete});
+  }
+  else
+  {
+    res.render('index', { id,title,date});
+  }
+}));
+          //TODO app
+//Check công việc đã hoàn thành
+router.get('/:id/done', asyncHandler(async function(req,res) {
+      const {id} = req.params;
+      const todos = await todo.findbyid(id);
+      if(todos){
+        await todos.markasdone();
+      }
+      res.redirect('/');
+}))
+// Thêm danh sách công việc cần làm
+router.post('/addtask', asyncHandler( async function(req,res){
+    var addtask = req.body.newtask;
+    todo.add(addtask,req.session.id);
+    res.redirect("/");
+}));
+module.exports = router;
