d1108347d1c1391d81d541d5404d1607d9384b98
Tran Do Thanh Hai <57478186+tdt-hai@users.noreply.github.com>
2020-05-26 15:05:55 +0700

    edit

--- Thuc_Hanh/Tuan_04/routes/index.js
+++ Thuc_Hanh/Tuan_04/routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const user = require('../services/user');
 const register = require('../services/registerhn');
+const { body, validationResult } = require('express-validator');
+const conference = require('../services/conference');
 
 const asyncHandler = require('express-async-handler');
 
@@ -26,10 +28,41 @@ router.get('/user', asyncHandler (async function(req,res){
 router.get('/admin', asyncHandler (async function(req,res){
   var id = await user.finduserbyid(req.session.id);
   const display = await register.findalluserhn();
-  //res.json(display);
-  res.render('admin',{id,display: display});
+  const display_conference = await conference.findallconference();
+  res.render('admin',{id,display: display,display_conference: display_conference });
 }));
-
+//Thêm thông tin hội nghị
+router.get('/addreference',asyncHandler(async function(req,res){
+  var id = await user.finduserbyid(req.session.id);
+  res.render('addreference',{id});
+}))
+router.post('/addreference',[
+  body('name')
+  .trim()
+  .notEmpty(),
+  body('content')
+  .trim()
+  .notEmpty(),
+  body('detail')
+  .trim()
+  .notEmpty(),
+  body('time')
+  .trim()
+  .notEmpty(),
+],asyncHandler(async function (req,res){
+  const errors = validationResult(req);
+  var id =  await user.finduserbyid(req.session.id);
+  if (!errors.isEmpty()) {
+      return res.status(422).render('addreference',{id,errors: errors.array()});
+  }
+  const conferences = await conference.create({
+      name: req.body.name,
+      content: req.body.content,
+      detail: req.body.detail,
+      time: req.body.time,
+  });
+  res.redirect('/');
+}));
 /*Profile */
 router.get('/profile', asyncHandler (async function(req,res){
   var id = await user.finduserbyid(req.session.id);
---
+++ Thuc_Hanh/Tuan_04/services/conference.js
@@ -0,0 +1,37 @@
+const bcrypt = require('bcrypt');
+const db = require('./db');
+const Sequelize = require('sequelize');
+
+/*model user*/
+const Model = Sequelize.Model;
+
+class Conference extends Model {
+    static async findallconference(){
+        return Conference.findAll();
+      }
+}
+Conference.init({
+  // attributes
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+    content: {
+        type: Sequelize.STRING,
+        allowNull: false
+        // allowNull defaults to true
+    },
+    detail: {
+        type: Sequelize.STRING,
+    },
+    time: {
+        type: Sequelize.DATE,
+    }
+}, 
+{
+  sequelize: db,
+  modelName: 'conference'
+});
+
+module.exports = Conference;
